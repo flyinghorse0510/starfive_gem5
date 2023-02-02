d1ee35d2fd9954633d4bb204a4a51a76b8e44503
zhiang <zhiang.li@starfivetech.com>
2023-02-02 18:01:02 +0800

    compress txsn trace log

--- traceMemTest.sh
+++ traceMemTest.sh
@@ -32,7 +32,7 @@ export WORKSPACE="$(pwd)/output"
 export GEM5_DIR=$(pwd)
 export ISA="RISCV"
 export CCPROT="CHI"
-export NUMCPUS=1
+export NUMCPUS=2
 
 if [ "$BUILD" != "" ]; then
     echo "Start building"
@@ -43,7 +43,7 @@ if [ "$RUN2" != "" ]; then
     OUTPUT_DIR="${WORKSPACE}/04_gem5dump/ExpMEMTest"
     mkdir -p $OUTPUT_DIR
     $GEM5_DIR/build/${ISA}_${CCPROT}/gem5.debug \
-        --debug-flags=IsolatedMemLatTest,RubySequencer,RubySlicc,MsgBufDebug,ProtocolTrace,RubyPort  --debug-file=debug.trace \
+        --debug-flags=TxnTrace  --debug-file=debug.trace \
         -d ${OUTPUT_DIR} \
         ${GEM5_DIR}/configs/example/isolated_ruby_mem_test.py \
         --num-dirs=1 \
@@ -52,10 +52,12 @@ if [ "$RUN2" != "" ]; then
         --topology=CustomMesh \
         --chi-config=${GEM5_DIR}/configs/example/noc_config/Starlink2.0_2x2Mesh.py \
         --ruby \
-        --maxloads=10 \
+        --maxloads=100 \
         --mem-size="4GB" \
         --num-cpus=${NUMCPUS}
     grep -rwI -e 'system\.ruby\.hnf\.cntrl' $OUTPUT_DIR/debug.trace > $OUTPUT_DIR/debug.hnf.trace
-    #sed -i '/triggerQueue/d' $OUTPUT_DIR/debug.trace
-    #sed -i '/system.ruby.network/d' $OUTPUT_DIR/debug.trace
+    # sed -i '/triggerQueue/d' $OUTPUT_DIR/debug.trace
+    # sed -i '/system.ruby.network/d' $OUTPUT_DIR/debug.trace
+    # sed -i '/reqRdy/d' $OUTPUT_DIR/debug.trace
+    sed -i '/triggerQueue/d; /system.ruby.network/d; /reqRdy/d' $OUTPUT_DIR/debug.trace
 fi
