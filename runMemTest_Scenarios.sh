64dadb10b3f0492e0cf004ddf2cc7602e2ed57d6
Zhiguo <zhiguo.ge@starfivetech.com>
2023-02-06 14:42:54 +0800

    enhance runMemTest_Scenarios.sh

--- runMemTest_Scenarios.sh
+++ runMemTest_Scenarios.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#command example: ./runMemTest_Scenarios.sh -t L2_Hit
+
 Help() {
    # Display Help
    echo "Run gem5 Starlink2.0 configurations."
@@ -16,18 +18,32 @@ BUILD=""
 RUN=""
 ANALYSIS=""
 
-while getopts "hbrsa" options; do
+TEST=""
+
+while getopts "hbrsat:" options; do
     case $options in
-        h) Help
-           exit;;
-        b) BUILD="yes"
-            ;;
-        r) RUN1="yes"
+       h) Help
+          exit;;
+       b) BUILD="yes"
            ;;
-        a) ANALYSIS="yes"
-           ;;
+       r) RUN1="yes"
+          ;;
+       a) ANALYSIS="yes"
+          ;;
+       t)
+          echo ${OPTARG}
+          TEST=${OPTARG}
+          echo "Processing option 'c' with '${OPTARG}' argument"
+          ;;
+     
     esac
 done
+
+if [ $TEST == "L2_Hit" ] || [ $TEST == "L1_Hit" ] || [ $TEST == "L3_Hit" ] || [ $TEST == "DDR_BW" ]; 
+then
+    RUN1="yes"
+    echo "to Run $TEST" 
+fi
 
 export WORKSPACE="$(pwd)/output"
 export GEM5_DIR=$(pwd)
@@ -41,13 +57,13 @@ buildType="gem5.opt"
 #1. To test latency need to set Sequencer maximal outstanding to 1
 #
 
-#TEST="L1_Hit"
-#TEST="L2_Hit"
-TEST="L3_Hit"
-#TEST="DDR_BW"
-
 if [ "$TEST" == "L1_Hit" ]; then
 echo $TEST
+
+#TEST="L1_Hit"
+#TEST="L2_Hit"
+#TEST="L3_Hit"
+#TEST="DDR_BW"
 
 l1d_size="32KiB"
 l1i_size="32KiB"
@@ -69,11 +85,12 @@ NUM_MEM=1
 TRANS_SET=(1 2 4)
 SNF_TBE_SET=(32 64)
 HNF_TBE=32
+NUM_LOAD_SET=(80000)
 
 #DEBUG_FLAGS=SeqMemLatTest,TxnTrace 
 #DEBUG_FLAGS=SeqMemLatTest
 DEBUG_FLAGS=PseudoInst
-OUTPUT_ROOT="${WORKSPACE}/GEM5_PDCP/MEMBW"
+OUTPUT_ROOT="${WORKSPACE}/GEM5_PDCP/MEMBW_Test"
 #OUTPUT_PREFIX="LLC_HIT_BW_MEM1_TestHNFTBE"
 #OUTPUT_PREFIX="SysClkRubyClk2GHz_LLC_HIT_BW_MEM1_HNF${HNF_TBE}"
 fi
@@ -94,13 +111,15 @@ NETWORK="simple" #"garnet" #"simple"
 
 DMT_Config=(True False)
 #NUM_CPU_SET=(1 2 4 8 16) # = #2 #4 #16
-NUM_CPU_SET=(4 8 16) # = #2 #4 #16
+#NUM_CPU_SET=(4 8 16) # = #2 #4 #16
+NUM_CPU_SET=(1) # = #2 #4 #16
 WKSET=8192 #131072 #8192 #16384 #524288 #(32768) #
 #NUM_MEM_SET=(1 2)
 NUM_MEM=1
 TRANS_SET=(1 2 4)
 SNF_TBE_SET=(32 64)
 HNF_TBE=32
+NUM_LOAD_SET=(4000) #no eviction/writeback. if more than 5000, more writeback/eviction
 
 DEBUG_FLAGS=PseudoInst
 OUTPUT_ROOT="${WORKSPACE}/GEM5_PDCP/MEMBW"
@@ -112,7 +131,7 @@ if [ "$TEST" == "L3_Hit" ]; then
 l1d_size="4KiB"
 l1i_size="4KiB"
 l2_size="64KiB"
-l3_size="32KiB" #"16KiB" #"1024KiB" #"256KiB"
+l3_size="1024KiB" #"32KiB" #"16KiB" #"1024KiB" #"256KiB"
 l1d_assoc=8
 l1i_assoc=8
 l2_assoc=8
@@ -122,13 +141,15 @@ NETWORK="simple" #"garnet" #"simple"
 
 DMT_Config=(True False)
 #NUM_CPU_SET=(1 2 4 8 16) # = #2 #4 #16
-NUM_CPU_SET=(4 8 16) # = #2 #4 #16
+NUM_CPU_SET=(1 2 4) # = #2 #4 #16
 WKSET=131072 #8192 #16384 #524288 #(32768) #
 #NUM_MEM_SET=(1 2)
 NUM_MEM=1
 TRANS_SET=(1 2 4)
 SNF_TBE_SET=(32 64)
 HNF_TBE=32
+NUM_LOAD_SET=(80000) #need more transactions. if just several thousand transaction, BW is low
+
 
 #DEBUG_FLAGS=SeqMemLatTest,TxnTrace 
 DEBUG_FLAGS=PseudoInst
@@ -156,8 +177,9 @@ WKSET=524288 #8192 #16384 #524288 #(32768) #
 #NUM_MEM_SET=(1 2)
 NUM_MEM=1
 TRANS_SET=(1 2 4)
-SNF_TBE_SET=(32 64)
+SNF_TBE_SET=(32)
 HNF_TBE=32
+NUM_LOAD_SET=(4000 80000)
 
 #DEBUG_FLAGS=SeqMemLatTest,TxnTrace 
 #DEBUG_FLAGS=SeqMemLatTest
@@ -165,19 +187,27 @@ DEBUG_FLAGS=PseudoInst
 OUTPUT_ROOT="${WORKSPACE}/GEM5_PDCP/MEMBW"
 fi
 
-DMT_Config=(False)
+DMT_Config=(True False)
 #NUM_CPU_SET=(1 2 4 8 16) # = #2 #4 #16
 #NUM_CPU_SET=(4 8 16) # = #2 #4 #16
-NUM_CPU_SET=(1) # = #2 #4 #16
+##NUM_CPU_SET=(2 4 8 16) # = #2 #4 #16
+
+#Sinlge Core
+#NUM_CPU_SET=(1) # = #2 #4 #16
+
+#Multi Core
+NUM_CPU_SET=(1 2 4 8 16) # = #2 #4 #16
+
 #WKSET=131072 #8192 #16384 #524288 #(32768) #
 #NUM_MEM_SET=(1 2)
 NUM_MEM=1
 TRANS_SET=(4)
 HNF_TBE=32
 SNF_TBE_SET=(32)
-NUM_LOAD_SET=(4000 5000 8000 80000)
+#NUM_LOAD_SET=(80000)
 
-DEBUG_FLAGS="SeqMemLatTest,TxnTrace"
+#DEBUG_FLAGS="SeqMemLatTest,TxnTrace"
+#DEBUG_FLAGS=TxnTrace
 
 OUTPUT_ROOT="${WORKSPACE}/GEM5_PDCP/MEM_Hier_BW"
 OUTPUT_PREFIX="TEST_${TEST}/NETWK${NETWORK}_LinkFactor40_SysClk2GHz"
@@ -189,7 +219,7 @@ fi
 
 if [ "$RUN1" != "" ]; then
     #OUTPUT_ROOT="${WORKSPACE}/04_gem5dump/HAS0.5_4x4_BW"
-    mkdir -p $OUTPUT_DIR
+    #mkdir -p $OUTPUT_DIR
     for DMT in ${DMT_Config[@]}; do
        for NUMCPUS in ${NUM_CPU_SET[@]}; do
           for TRANS in ${TRANS_SET[@]}; do
@@ -252,16 +282,18 @@ fi
           echo $OUTPUT_DIR
           grep simTicks  ${statsfile}
           grep "mem_ctrls" ${statsfile} | grep readReqs
-          #grep "l1d.cache.numDataArrayReads" ${statsfile}
-          #grep "l1d.cache.numTagArrayReads" ${statsfile} 
+          ##grep "l1d.cache.numDataArrayReads" ${statsfile}
+          ##grep "l1d.cache.numTagArrayReads" ${statsfile} 
           grep "l1d.cache.m_demand" ${statsfile} 
-          grep "l1d.avg_size" ${statsfile} | grep "TBE Request Occupancy"
+          #grep "l1d.avg_size" ${statsfile} | grep "TBE Request Occupancy"
  
           grep "l2.cache.m_demand" ${statsfile}  
           #grep "cache.m_demand" ${statsfile} | grep hnf
           grep "cntrl.avg_size" ${statsfile} | grep "TBE Request Occupanc"
           grep "m_outstandReqHistSeqr::mean" ${statsfile}
           grep "snf.cntrl.avg_size" ${statsfile}
+          grep "mem_ctrls.readReqs" ${statsfile}
+          grep "mem_ctrls.writeReqs" ${statsfile}
          done
        done
      done
