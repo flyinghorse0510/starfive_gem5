8375443f7f446172abdae13ae3d3d10730b27699
Arka <arka.maity@starfivetech.com>
2023-02-07 18:14:24 +0800

    Producer consumer Transition stats are reasonable. C2C benchmarks numbers to begin

--- runProducerConsumerTest.sh
+++ runProducerConsumerTest.sh
@@ -4,23 +4,28 @@ Help() {
    # Display Help
    echo "Run gem5 Starlink2.0 configurations."
    echo
-   echo "Syntax: scriptTemplate [-b|r|h]"
+   echo "Syntax: scriptTemplate [-b|r|h|a]"
    echo "options:"
    echo "h     Print this Help."
    echo "b     Build."
    echo "r     Run1 (MemTest)."
+   echo "a     Analyze/Grep"
    echo
 }
 
 BUILD=""
 RUN=""
-while getopts "hbrs" options; do
+ANALYSIS=""
+
+while getopts "hbra" options; do
     case $options in
         h) Help
            exit;;
         b) BUILD="yes"
             ;;
         r) RUN1="yes"
+           ;;
+        a) ANALYSIS="yes"
            ;;
     esac
 done
@@ -29,19 +34,32 @@ export WORKSPACE="$(pwd)/output"
 export GEM5_DIR=$(pwd)
 export ISA="RISCV"
 export CCPROT="CHI"
-export NUMCPUS=3
 
 buildType="gem5.opt"
-l1d_size="32KiB"
-l1i_size="32KiB"
-l2_size="128KiB"
-l3_size="1024KiB" #"256KiB"
-l1d_assoc=8
-l1i_assoc=8
-l2_assoc=8
-l3_assoc=16
-NUM_LLC=16
-NETWORK="garnet" #"simple"
+l1d_size="256B"
+l1i_size="256B"
+l2_size="32KiB"
+l3_size="1024KiB" #"16KiB" #"1024KiB" #"256KiB"
+l1d_assoc=2
+l1i_assoc=2
+l2_assoc=2
+l3_assoc=8
+NUM_LLC=2
+NETWORK="simple" #"garnet" #"simple"
+
+DMT_Config=(False)
+NUM_CPU_SET=(2) # = #2 #4 #16
+WKSET=384 #2560 #3072 #8192 #16384 #524288 #(32768) #
+NUM_MEM_SET=(1)
+NUM_MEM=1
+TRANS_SET=(1)
+
+#DEBUG_FLAGS=SeqMemLatTest,TxnTrace 
+#DEBUG_FLAGS=SeqMemLatTest
+DEBUG_FLAGS=ProdConsMemLatTest
+OUTPUT_ROOT="${WORKSPACE}/GEM5_PDCP/C2C"
+OUTPUT_PREFIX="PRODCONS"
+ADDR_GEN_CONFIG=(True) # If True address generation is interleaved, otherwise strided
 
 if [ "$BUILD" != "" ]; then
     echo "Start building"
@@ -49,36 +67,69 @@ if [ "$BUILD" != "" ]; then
 fi
 
 if [ "$RUN1" != "" ]; then
-    OUTPUT_DIR="${WORKSPACE}/04_gem5dump/HAS0.5_4x4_2DDR"
     mkdir -p $OUTPUT_DIR
-    $GEM5_DIR/build/${ISA}_${CCPROT}/${buildType} \
-        --debug-flags=ProdConsMemLatTest --debug-file=debug.trace \
-        -d ${OUTPUT_DIR} \
-        ${GEM5_DIR}/configs/example/seq_ruby_mem_test.py \
-        --num-dirs=2 \
-        --num-l3caches=4 \
-        --l1d_size=${l1d_size} \
-        --l1i_size=${l1i_size} \
-        --l2_size=${l2_size} \
-        --l3_size=${l3_size} \
-        --l1d_assoc=${l1d_assoc} \
-        --l1i_assoc=${l1i_assoc} \
-        --l2_assoc=${l2_assoc} \
-        --l3_assoc=${l3_assoc} \
-        --network=${NETWORK} \
-        --topology=CustomMesh \
-        --chi-config=${GEM5_DIR}/configs/example/noc_config/Starlink2.0_4x4Mesh.py \
-        --ruby \
-        --maxloads=100 \
-        --mem-size="16GB" \
-        --size-ws=64 \
-        --mem-type=DDR4_3200_8x8 \
-        --addr-mapping="RoRaBaBg1CoBg0Co53Dp" \
-        --mem-test-type='prod_cons_test' \
-        --disable-gclk-set \
-        --enable-DMT=False \
-        --num-cpus=${NUMCPUS} \
-        --num-producers=1
-    grep -rwI -e 'system\.cpu0' $OUTPUT_DIR/debug.trace > $OUTPUT_DIR/debug.cpu0.trace
-    grep -rwI -e 'system\.cpu1' $OUTPUT_DIR/debug.trace > $OUTPUT_DIR/debug.cpu1.trace
+    for DMT in ${DMT_Config[@]}; do
+       for NUMCPUS in ${NUM_CPU_SET[@]}; do
+          for TRANS in ${TRANS_SET[@]}; do
+            for ADDRGEN in ${ADDR_GEN_CONFIG[@]}; do
+              OUTPUT_DIR="${OUTPUT_ROOT}/${OUTPUT_PREFIX}/WS${WKSET}_Core${NUMCPUS}_L1${l1d_size}_L2${l2_size}_L3${l3_size}_MEM${NUM_MEM}_DMT${DMT}_TRANS${TRANS}_ADDRGEN${ADDRGEN}"
+              mkdir -p ${OUTPUT_DIR}
+              $GEM5_DIR/build/${ISA}_${CCPROT}/${buildType} \
+                 --debug-flags=$DEBUG_FLAGS --debug-file=debug.trace \
+                 -d $OUTPUT_DIR \
+                 ${GEM5_DIR}/configs/example/seq_ruby_mem_test.py \
+                 --num-dirs=${NUM_MEM} \
+                 --num-l3caches=${NUM_LLC} \
+                 --l1d_size=${l1d_size} \
+                 --l1i_size=${l1i_size} \
+                 --l2_size=${l2_size} \
+                 --l3_size=${l3_size} \
+                 --l1d_assoc=${l1d_assoc} \
+                 --l1i_assoc=${l1i_assoc} \
+                 --l2_assoc=${l2_assoc} \
+                 --l3_assoc=${l3_assoc} \
+                 --network=${NETWORK} \
+                 --topology=CustomMesh \
+                 --chi-config=${GEM5_DIR}/configs/example/noc_config/Starlink2.0_4x4Mesh.py \
+                 --ruby \
+                 --maxloads=10 \
+                 --mem-size="16GB" \
+                 --size-ws=${WKSET} \
+                 --mem-type=DDR4_3200_8x8 \
+                 --addr-mapping="RoRaBaBg1CoBg0Co53Dp" \
+                 --mem-test-type='prod_cons_test' \
+                 --disable-gclk-set \
+                 --enable-DMT=${DMT} \
+                 --num_trans_per_cycle_llc=${TRANS} \
+                 --num-cpus=${NUMCPUS} \
+                 --num-producers=1 \
+                 --addr-intrlvd-or-tiled=${ADDRGEN}
+            done
+          done
+        done
+    done
+fi
+
+if [ "$ANALYSIS" != "" ]; then
+    for DMT in ${DMT_Config[@]}; do
+       for NUMCPUS in ${NUM_CPU_SET[@]}; do
+          for TRANS in ${TRANS_SET[@]}; do
+            for ADDRGEN in ${ADDR_GEN_CONFIG[@]}; do
+
+              OUTPUT_DIR="${OUTPUT_ROOT}/${OUTPUT_PREFIX}/WS${WKSET}_Core${NUMCPUS}_L1${l1d_size}_L2${l2_size}_L3${l3_size}_MEM${NUM_MEM}_DMT${DMT}_TRANS${TRANS}_ADDRGEN${ADDRGEN}"
+              statsfile=$OUTPUT_DIR/stats.txt
+              OUTPUT_FILE="${OUTPUT_ROOT}/WS${WKSET}_Core${NUMCPUS}_L1${l1d_size}_L2${l2_size}_L3${l3_size}_MEM${NUM_MEM}_DMT${DMT}_TRANS${TRANS}_ADDRGEN${ADDRGEN}.csv"
+              grep "simTicks" ${statsfile} > ${OUTPUT_FILE}
+              # grep "cache.m_demand_hits" ${statsfile} | grep "l2" >> ${OUTPUT_FILE}
+              # grep "cache.m_demand_accesses" ${statsfile} | grep "l2" >> ${OUTPUT_FILE}
+              # grep "cache.m_demand_hits" ${statsfile} | grep "hnf" >> ${OUTPUT_FILE}
+              # grep "cache.m_demand_accesses" ${statsfile} | grep "hnf" >> ${OUTPUT_FILE}
+              # grep "cntrl.snpOut.m_msg_count" ${statsfile} | grep "hnf" >> ${OUTPUT_FILE}
+              grep "inTransLatHist" ${statsfile} | grep "\:\:total"  | grep -E "cpu[0-9]+\.l2" >> ${OUTPUT_FILE}
+              echo "" >> ${OUTPUT_FILE}
+              grep "inTransLatHist" ${statsfile} | grep "\:\:total"  | grep -E "hnf" >> ${OUTPUT_FILE}
+            done
+          done
+        done
+    done
 fi
