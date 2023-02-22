7b0fddf47ce921e5b3d3498b44659ebda46a5cad
Arka <arka.maity@starfivetech.com>
2023-02-22 10:24:38 +0800

    [wip]: Added Source code of SnoopFilter. Currently a copy paste of RubyCache

--- src/mem/ruby/structures/RubySnoopFilter.py
+++ src/mem/ruby/structures/RubySnoopFilter.py
@@ -1,18 +1,4 @@
-# -*- mode:python -*-
-
-# Copyright (c) 2021 ARM Limited
-# All rights reserved.
-#
-# The license below extends only to copyright in the software and shall
-# not be construed as granting a license to any other intellectual
-# property including but not limited to intellectual property relating
-# to a hardware implementation of the functionality of the software
-# licensed hereunder.  You may use the software subject to the license
-# terms below provided that you ensure that this notice is replicated
-# unmodified and in its entirety in all distributions of the software,
-# modified or unmodified, in source code or in binary form.
-#
-# Copyright (c) 2012 Mark D. Hill and David A. Wood
+# Copyright (c) 2009 Advanced Micro Devices, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -38,26 +24,26 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-Import('*')
+from m5.params import *
+from m5.proxy import *
+from m5.objects.ReplacementPolicies import *
+from m5.SimObject import SimObject
 
-if env['CONF']['PROTOCOL'] == 'None':
-    Return()
+class RubySnoopFilter(SimObject):
+    type = 'RubySnoopFilter'
+    cxx_class = 'gem5::ruby::RubySnoopFilter'
+    cxx_header = "mem/ruby/structures/SnoopFilter.hh"
 
-SimObject('RubyCache.py', sim_objects=['RubyCache'])
-SimObject('DirectoryMemory.py', sim_objects=['RubyDirectoryMemory'])
-SimObject('RubyPrefetcher.py', sim_objects=['RubyPrefetcher'])
-SimObject('WireBuffer.py', sim_objects=['RubyWireBuffer'])
+    size = Param.MemorySize("capacity in bytes");
+    assoc = Param.Int("");
+    replacement_policy = Param.BaseReplacementPolicy(TreePLRURP(), "")
+    start_index_bit = Param.Int(6, "index start, default 6 for 64-byte line");
+    is_icache = Param.Bool(False, "is instruction only cache");
+    block_size = Param.MemorySize("0B", "block size in bytes. 0 means default RubyBlockSize")
 
-SimObject('RubySnoopFilter.py', sim_objects=['RubySnoopFilter'])
-
-Source('SnoopFilter.cc')
-Source('DirectoryMemory.cc')
-Source('CacheMemory.cc')
-Source('WireBuffer.cc')
-Source('PersistentTable.cc')
-Source('RubyPrefetcher.cc')
-Source('TimerTable.cc')
-Source('BankedArray.cc')
-Source('TBEStorage.cc')
-if env['PROTOCOL'] == 'CHI':
-    Source('MN_TBETable.cc')
+    dataArrayBanks = Param.Int(1, "Number of banks for the data array")
+    tagArrayBanks = Param.Int(1, "Number of banks for the tag array")
+    dataAccessLatency = Param.Cycles(1, "cycles for a data array access")
+    tagAccessLatency = Param.Cycles(1, "cycles for a tag array access")
+    resourceStalls = Param.Bool(False, "stall if there is a resource failure")
+    ruby_system = Param.RubySystem(Parent.any, "")
