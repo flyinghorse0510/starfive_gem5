ccfb261b46aa600b5501bbe9e539f9262b9b233e
Arka <arka.maity@starfivetech.com>
2023-03-20 10:33:57 +0800

    Added missing MemTest flags in producer-consumer Memtest, discovered during regression tests

--- runSnoopFilterIsoTest.sh
+++ runSnoopFilterIsoTest.sh
@@ -39,7 +39,7 @@ WORKSPACE="$(pwd)/output"
 GEM5_DIR=$(pwd)
 ISA="RISCV"
 CCPROT="CHI"
-BUILDTYPE="gem5.debug"
+BUILDTYPE="gem5.opt"
 OUTPUT_ROOT="${WORKSPACE}/GEM5_PDCP/SnoopFilterTest"
 PY3=/home/arka.maity/anaconda3/bin/python3
 
@@ -102,19 +102,50 @@ fi
 if [ "$PRODCONSTEST" != "" ]; then
     LOC_CONS=1
     LOC_PROD=0
-    MAXNUMLOADS=1
-    WKSET=65536
-    DCT=False
+    l1d_size="128B"
+    l1i_size="128B"
+    l2_size="8KiB"
+    l3_size="1KiB"
+    l1d_assoc=1
+    l1i_assoc=1
+    l2_assoc=4
+    l3_assoc=8
+    NUM_LLC=1
+    SEQ_TBE=32
+    LoadFactor=10
+    NUM_MEM=4
+    NUM_DDR_XP=2
+    NUM_DDR_Side=1
     LINK_BW=16
-    OUTPUT_DIR="${WORKSPACE}/MOD0.5_SnoopFilter_ProdConsTest"
-    mkdir -p $OUTPUT_DIR
+    LINKWIDTH=256
+    VC_PER_VNET=2
+    LINK_LAT=1
+    ROUTER_LAT=0
+    DMT=False
+    DCT=False
+    HNF_TBE=32
+    SNF_TBE=32
+    SEQ_TBE=32
+    TRANS=4
+    MultiCoreAddrMode=True
+    NETWORK="simple"
+    SNOOP_FILTER_SIZE=2
+    SNOOP_FILTER_ASSOC=1
+    NUMCPUS=16
     DEBUGFLAGS=RubyGenerated,RubyCHIDebugStr5,ProdConsMemLatTest,TxnTrace
+    
+    WKSETLIST=(2048)
+    for WKSET in ${WKSETLIST[@]}; do
+    OUTPUT_PREFIX="ProdCons_${NETWORK}"
+    OUTPUT_DIR="${OUTPUT_ROOT}/${OUTPUT_PREFIX}/WS${WKSET}_Core${NUMCPUS}_L1${l1d_size}_L2${l2_size}_L3${l3_size}_LoadFactor${LoadFactor}_SFSize${SNOOP_FILTER_SIZE}_SFAssoc${SNOOP_FILTER_ASSOC}" 
     $GEM5_DIR/build/${ISA}_${CCPROT}/${BUILDTYPE} \
         --debug-flags=$DEBUGFLAGS --debug-file=debug.trace \
         -d $OUTPUT_DIR \
         ${GEM5_DIR}/configs/example/seq_ruby_mem_test.py \
         --ruby \
-        --num-dirs=1 \
+        --num-dirs=${NUM_MEM} \
+        --DDR-loc-num=${NUM_DDR_XP} \
+        --DDR-side-num=${NUM_DDR_Side} \
         --num-l3caches=${NUM_LLC} \
         --l1d_size=${l1d_size} \
         --l1i_size=${l1i_size} \
@@ -128,27 +159,36 @@ if [ "$PRODCONSTEST" != "" ]; then
         --topology=CustomMesh \
         --simple-physical-channels \
         --simple-link-bw-factor=${LINK_BW} \
-        --chi-config=${GEM5_DIR}/configs/example/noc_config/Starlink2.0_2x2Mesh.py \
+        --link-width-bits=${LINKWIDTH} \
+        --vcs-per-vnet=${VC_PER_VNET} \
+        --link-latency=${LINK_LAT} \
+        --router-latency=${ROUTER_LAT} \
+        --chi-config=${GEM5_DIR}/configs/example/noc_config/Starlink2.0_4x4Mesh.py \
         --mem-size="16GB" \
         --mem-type=DDR4_3200_8x8 \
         --addr-mapping="RoRaBaBg1CoBg0Co53Dp" \
         --mem-test-type='prod_cons_test' \
         --disable-gclk-set \
-        --enable-DMT=False \
+        --enable-DMT=${DMT} \
         --enable-DCT=${DCT} \
-        --num_trans_per_cycle_llc=4 \
-        --addr-intrlvd-or-tiled=True \
+        --num_trans_per_cycle_llc=${TRANS} \
+        --num-HNF-TBE=${HNF_TBE}  \
+        --num-SNF-TBE=${SNF_TBE}  \
+        --addr-intrlvd-or-tiled=$MultiCoreAddrMode \
         --bench-c2cbw-mode=True \
-        --maxloads=${MAXNUMLOADS} \
+        --sequencer-outstanding-requests=${SEQ_TBE} \
+        --maxloads=${LoadFactor} \
         --size-ws=${WKSET} \
         --num-cpus=${NUMCPUS} \
-        --sequencer-outstanding-requests=32 \
         --chs-1p1c \
         --chs-cons-id=${LOC_CONS} \
         --chs-prod-id=${LOC_PROD} \
-        --num-snoopfilter-entries=1024 \
-        --num-snoopfilter-assoc=2
-    grep -E 'system.cpu0:|system.cpu0.data_sequencer:' $OUTPUT_DIR/debug.trace > $OUTPUT_DIR/debug.cpu0.trace
+        --num-snoopfilter-entries=${SNOOP_FILTER_SIZE} \
+        --num-snoopfilter-assoc=${SNOOP_FILTER_ASSOC}
+    done
+    grep -E 'hnf' ${OUTPUT_DIR}/debug.trace > ${OUTPUT_DIR}/debug.hnf.trace
+    grep -E 'Complete,R|Start,R|Reader finished generating' ${OUTPUT_DIR}/debug.trace > ${OUTPUT_DIR}/debug.txncomplete.trace
+    # grep -E 'system.cpu0:|system.cpu0.data_sequencer:' $OUTPUT_DIR/debug.trace > $OUTPUT_DIR/debug.cpu0.trace
 fi
 
 if [ "$GATETEST" != "" ]; then
@@ -236,7 +276,6 @@ if [ "$GATETEST" != "" ]; then
           --num-snoopfilter-entries=${SNOOP_FILTER_SIZE} \
           --num-snoopfilter-assoc=${SNOOP_FILTER_ASSOC} \
           --num-producers=1
-          
           
           grep -E 'hnf' ${OUTPUT_DIR}/debug.trace > ${OUTPUT_DIR}/debug.hnf.trace
           grep -E 'CacheEntry Victim Busy|SnoopFilter Victim Busy|CacheEntry Undergoing SFRepl' ${OUTPUT_DIR}/debug.hnf.trace > ${OUTPUT_DIR}/debug.filt.trace
