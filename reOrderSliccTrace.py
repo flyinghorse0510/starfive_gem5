2a6e047cbb5d84ac22b9459e632343054f35c77b
Arka <arka.maity@starfivetech.com>
2023-05-19 13:52:59 +0800

    Generate CSV files of per address transactions from the debug trace

--- reOrderSliccTrace.py
+++ reOrderSliccTrace.py
@@ -6,11 +6,12 @@ import ast
 import pandas as pd
 import numpy as np
 
-def getTransitionsActions(logFile,dumpFile):
+def getTransitionsActions(logFile,dumpFile,shorten=True):
     transitionPat=re.compile(r'^(\s*\d*): (\S+): \[Cache_Controller ([0-9]+)\], Time: ([0-9]+), state: ([\w]+), event: ([\w]+), addr: ([0-9a-fx]+)')
-    actionPat=re.compile(r'^(\s*\d*): (\S+): executing ([\w]+)')
-    nextStatePat=re.compile(r'^(\s*\d*): (\S+): next_state: ([\w]+)')
+    actionPat=re.compile(r'^(\s*\d*): (\S+): addr: ([0-9a-fx]+), executing ([\w]+)')
+    nextStatePat=re.compile(r'^(\s*\d*): (\S+): addr: ([0-9a-fx]+), next_state: ([\w]+)')
     tbeStatePat=re.compile(r'^(\s*\d*): (\S+): ([\S]+):([\d]+): TBEState')
+    stallPat=re.compile(r'^(\s*\d*): (\S+): ([\S]+):([\d]+): addr: ([0-9a-fx]+), Stalled reqType: ([\w]+), dest: ([\S\d]+)')
     addrDict=dict() # Each address consist of a list of transitions and actions
     addr='INVALID'
     tickPerCyc=500
@@ -21,6 +22,7 @@ def getTransitionsActions(logFile,dumpFile):
             actionMatch=actionPat.match(line)
             nextStateMatch=nextStatePat.match(line)
             tbeStateMatch=tbeStatePat.match(line)
+            stallMatch=stallPat.match(line)
             if tranMatch :
                 addr=tranMatch.group(7)
                 initState=tranMatch.group(5)
@@ -43,14 +45,23 @@ def getTransitionsActions(logFile,dumpFile):
             elif actionMatch :
                 assert(addr != 'INVALID')
                 assert(addr in addrDict)
-                action=actionMatch.group(3)
+                addr=actionMatch.group(3)
+                action=actionMatch.group(4)
                 addrDict[addr][-1]['actions'].append(action)
             elif nextStateMatch :
                 assert(addr != 'INVALID')
                 assert(addr in addrDict)
-                addrDict[addr][-1]['final']=nextStateMatch.group(3)
+                addr=nextStateMatch.group(3)
+                addrDict[addr][-1]['final']=nextStateMatch.group(4)
+            elif stallMatch:
+                addr=stallMatch.group(5)
+                assert(addr in addrDict)
+                addrDict[addr][-1]['final']='STALL'
     with open(dumpFile,'w') as fw:
-        print(f'Cyc,Addr,Agent,InitState,Event,NextState,Action',file=fw)
+        if shorten:
+            print(f'Cyc,Addr,Agent,InitState,Event,NextState',file=fw)
+        else :
+            print(f'Cyc,Addr,Agent,InitState,Event,NextState,Action',file=fw)
         for k,vl in addrDict.items():
             addr=k
             for v in vl:
@@ -59,18 +70,23 @@ def getTransitionsActions(logFile,dumpFile):
                 init=v['init']
                 event=v['event']
                 final=v['final']
-                for action in v['actions'] :
-                    print(f'{cyc},{addr},{agent},{init},{event},{final},{action}',file=fw)
+                if shorten:
+                    print(f'{cyc},{addr},{agent},{init},{event},{final}',file=fw)
+                else :
+                    for action in v['actions'] :
+                        print(f'{cyc},{addr},{agent},{init},{event},{final},{action}',file=fw)
+                
 
 
 def main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--input',required=True,type=str)
     parser.add_argument('--output',required=True,type=str)
+    parser.add_argument('--shorten',type=ast.literal_eval,required=True,help='Ignore the action prints')
     options=parser.parse_args()
     logFile=options.input
     dumpFile=options.output
-    getTransitionsActions(logFile,dumpFile)
+    getTransitionsActions(logFile,dumpFile,options.shorten)
 
 if __name__=="__main__":
     main()
