5f16f33cdbffc4538f0380bd848778a7ae41e3fe
Arka <arka.maity@starfivetech.com>
2023-07-04 15:34:16 +0800

    Cleaned up stats_parser to accept ENV parameters from set dump, instead of the command line

--- runMSHRSetConflicts.sh
+++ runMSHRSetConflicts.sh
@@ -38,9 +38,6 @@ if [ "$BUILD" != "" ]; then
 fi
 
 
-# --debug-start=383302001
-# --debug-end=393302001
-
 if [ "$RUN" != "" ]; then
     l1d_size="4KiB"
     l1i_size="4KiB"
@@ -68,9 +65,10 @@ if [ "$RUN" != "" ]; then
     TRANS=4
     MultiCoreAddrMode=True
     NETWORK="simple"
-    DEBUGFLAGS=RubyResourceStalls,RubyTxnTrace,TxnTrace,RubyCHIDebugStr5,SimpleNetworkDebug
-    # DEBUGFLAGS=SeqMemLatTest
-    OUTPUT_PREFIX="MSHRSetConflicts3_LargeLLC_Deadlocks"
+    # DEBUGFLAGS=RubyResourceStalls,RubyTxnTrace,TxnTrace,RubyCHIDebugStr5,SimpleNetworkDebug
+    # DEBUGFLAGS=RubyResourceStalls,RubyTxnTrace,TxnTrace,RubyCHIDebugStr5
+    DEBUGFLAGS=SeqMemLatTest
+    OUTPUT_PREFIX="MSHR_RetryImpl2"
     XOR_ADDR_BITS=4
     RANDOMIZE_ACC=False
     BLOCK_STRIDE_BITS=0
@@ -92,128 +90,93 @@ if [ "$RUN" != "" ]; then
     CONFIG_BENCHNAME=("bw_test_sf")
     CONFIG_SLOTS_BLOCKED_BY_SET=(False)
 
-    # for l3_size in ${L3_SIZE_CONFIG[@]}; do
-    #     for NUMCPUS in ${CONFIG_NUMCPUS[@]}; do
-    #         for BENCHNAME in ${CONFIG_BENCHNAME[@]}; do
-    #            for SLOTS_BLOCKED_BY_SET in ${CONFIG_SLOTS_BLOCKED_BY_SET[@]}; do
-    #                 if [ $BENCHNAME == "bw_test_sf" ]; then
-    #                     # CONFIG_READ_WRITE_RATIO=('1-0' '0-1')
-    #                     CONFIG_READ_WRITE_RATIO=('0-1')
-    #                 elif [ $BENCHNAME == "memcpy_test" ]; then
-    #                     CONFIG_READ_WRITE_RATIO=('0-1')
-    #                 fi
-    #                 for READ_WRITE_RATIO in ${CONFIG_READ_WRITE_RATIO[@]}; do
-	# 		            WKSET=$((${NUM_LLC}*${l3_size}*1024*2))
-    #                     L3_SIZE_KB="${l3_size}KiB"
-    #                     OUTPUT_BASE="WS${WKSET}_Core${NUMCPUS}_L3${L3_SIZE_KB}_ReadWrite${READ_WRITE_RATIO}_MSHRSlotsBlockedBySet_${SLOTS_BLOCKED_BY_SET}_Bench_${BENCHNAME}"
-    #                     OUTPUT_DIR="${OUTPUT_ROOT}/${OUTPUT_PREFIX}/${OUTPUT_BASE}"
-    #                     echo "GateTest Started: ${OUTPUT_BASE}"
-    #                     mkdir -p ${OUTPUT_DIR}
-    #                     set > ${OUTPUT_DIR}/Variables.txt
-    #                     $GEM5_DIR/build/${ISA}_${CCPROT}/${BUILDTYPE} \
-    #                         --debug-flags=$DEBUGFLAGS \
-    #                         --debug-file=debug.trace \
-    #                         --debug-start=383302001 \
-    #                         --debug-end=393302001 \
-    #                         -d $OUTPUT_DIR \
-    #                         ${GEM5_DIR}/configs/example/seq_ruby_mem_test.py \
-    #                         --chi-data-width=${CHI_DATA_WIDTH} \
-    #                         --num-dirs=${NUM_MEM} \
-    #                         --DDR-loc-num=${NUM_DDR_XP} \
-    #                         --DDR-side-num=${NUM_DDR_Side} \
-    #                         --num-l3caches=${NUM_LLC} \
-    #                         --l1d_size=${l1d_size} \
-    #                         --l1i_size=${l1i_size} \
-    #                         --l2_size=${l2_size} \
-    #                         --l3_size=${L3_SIZE_KB} \
-    #                         --l1d_assoc=${l1d_assoc} \
-    #                         --l1i_assoc=${l1i_assoc} \
-    #                         --l2_assoc=${l2_assoc} \
-    #                         --l3_assoc=${l3_assoc} \
-    #                         --network=${NETWORK} \
-    #                         --buffer-size=${BUFFER_SIZE} \
-    #                         --link-width-bits=${LINKWIDTH} \
-    #                         --simple-ext-link-bw-factor=$(($LINKWIDTH/8)) \
-    #                         --simple-int-link-bw-factor=$(($LINKWIDTH/8)) \
-    #                         --vcs-per-vnet=${VC_PER_VNET} \
-    #                         --link-latency=${LINK_LAT} \
-    #                         --router-latency=${ROUTER_LAT} \
-    #                         --topology=CustomMesh \
-    #                         --simple-physical-channels \
-    #                         --chi-config=${GEM5_DIR}/configs/example/noc_config/Starlink2.0_4x4Mesh.py \
-    #                         --ruby \
-    #                         --maxloads=${LoadFactor} \
-    #                         --mem-size="16GB" \
-    #                         --size-ws=${WKSET} \
-    #                         --mem-type=DDR4_3200_8x8 \
-    #                         --addr-mapping="RoRaBaBg1CoBg0Co53Dp" \
-    #                         --mem-test-type=$BENCHNAME \
-    #                         --addr-intrlvd-or-tiled=$MultiCoreAddrMode  \
-    #                         --disable-gclk-set \
-    #                         --enable-DMT=${DMT} \
-    #                         --enable-DCT=${DCT} \
-    #                         --num-HNF-TBE=${HNF_TBE} \
-    #                         --ratio-repl-req-TBE=$PART_RATIO \
-    #                         --part-TBEs=$PART_TBE \
-    #                         --num-SNF-TBE=${SNF_TBE}  \
-    #                         --sequencer-outstanding-requests=${SEQ_TBE} \
-    #                         --num_trans_per_cycle_llc=${TRANS} \
-    #                         --num-cpus=${NUMCPUS} \
-    #                         --num-dmas=0 \
-    #                         --inj-interval=1 \
-    #                         --num-snoopfilter-entries=${SNOOP_FILTER_SIZE} \
-    #                         --num-snoopfilter-assoc=${SNOOP_FILTER_ASSOC} \
-    #                         --allow-infinite-SF-entries=${IDEAL_SNOOP_FILTER} \
-    #                         --xor-addr-bits=${XOR_ADDR_BITS} \
-    #                         --block-stride-bits=${BLOCK_STRIDE_BITS} \
-    #                         --randomize-acc=${RANDOMIZE_ACC} \
-    #                         --ratio-read-write=${READ_WRITE_RATIO} \
-    #                         --slots_bocked_by_set=${SLOTS_BLOCKED_BY_SET} \
-    #                         --chi-buffer-depth=16 \
-    #                         --chi-buffer-max-deq-rate=1 \
-    #                         --num-producers=1 > ${OUTPUT_DIR}/cmd.log 2>&1 &
-    #                 done
-    #             done
-    #         done
-    #     done
-    #     wait
-    #     echo "------------------------------------------------"
-    # done
-
     for l3_size in ${L3_SIZE_CONFIG[@]}; do
         for NUMCPUS in ${CONFIG_NUMCPUS[@]}; do
-            for BENCHNAME in ${CONFIG_BENCHNAME[@]}; do
+            for BENCHMARK in ${CONFIG_BENCHNAME[@]}; do
                for SLOTS_BLOCKED_BY_SET in ${CONFIG_SLOTS_BLOCKED_BY_SET[@]}; do
-                    if [ $BENCHNAME == "bw_test_sf" ]; then
+                    if [ $BENCHMARK == "bw_test_sf" ]; then
                         # CONFIG_READ_WRITE_RATIO=('1-0' '0-1')
                         CONFIG_READ_WRITE_RATIO=('0-1')
-                    elif [ $BENCHNAME == "memcpy_test" ]; then
+                    elif [ $BENCHMARK == "memcpy_test" ]; then
                         CONFIG_READ_WRITE_RATIO=('0-1')
                     fi
                     for READ_WRITE_RATIO in ${CONFIG_READ_WRITE_RATIO[@]}; do
-                        WKSET=$((${NUM_LLC}*${l3_size}*1024*2))
+			            WKSET=$((${NUM_LLC}*${l3_size}*1024*2))
                         L3_SIZE_KB="${l3_size}KiB"
-                        OUTPUT_BASE="WS${WKSET}_Core${NUMCPUS}_L3${L3_SIZE_KB}_ReadWrite${READ_WRITE_RATIO}_MSHRSlotsBlockedBySet_${SLOTS_BLOCKED_BY_SET}_Bench_${BENCHNAME}"
+                        OUTPUT_BASE="WS${WKSET}_Core${NUMCPUS}_L3${L3_SIZE_KB}_ReadWrite${READ_WRITE_RATIO}_MSHRSlotsBlockedBySet_${SLOTS_BLOCKED_BY_SET}_Bench_${BENCHMARK}"
                         OUTPUT_DIR="${OUTPUT_ROOT}/${OUTPUT_PREFIX}/${OUTPUT_BASE}"
-                        # for llc in $(seq 0 15); do
-                        #     echo "grep filtering: ${OUTPUT_BASE}/${llc}"
-                        #     if [ $llc -lt 10 ]; then
-                        #         grep -E "system.ruby.hnf0${llc}.cntrl" ${OUTPUT_DIR}/debug.trace > ${OUTPUT_DIR}/debug.hnf0${llc}.trace &
-                        #     else 
-                        #         grep -E "system.ruby.hnf${llc}.cntrl" ${OUTPUT_DIR}/debug.trace > ${OUTPUT_DIR}/debug.hnf${llc}.trace &
-                        #     fi
-                        # done
-                        # wait
-                        ${PY3} parseRouterIncomingMsg.py \
-                              --input ${OUTPUT_DIR} \
-                              --input-sw-port ${GEM5_DIR}/SwitchPortLinkMapping.txt
+                        echo "GateTest Started: ${OUTPUT_BASE}"
+                        mkdir -p ${OUTPUT_DIR}
+                        set > ${OUTPUT_DIR}/Variables.txt
+                        $GEM5_DIR/build/${ISA}_${CCPROT}/${BUILDTYPE} \
+                            --debug-flags=$DEBUGFLAGS \
+                            --debug-file=debug.trace \
+                            -d $OUTPUT_DIR \
+                            ${GEM5_DIR}/configs/example/seq_ruby_mem_test.py \
+                            --chi-data-width=${CHI_DATA_WIDTH} \
+                            --num-dirs=${NUM_MEM} \
+                            --DDR-loc-num=${NUM_DDR_XP} \
+                            --DDR-side-num=${NUM_DDR_Side} \
+                            --num-l3caches=${NUM_LLC} \
+                            --l1d_size=${l1d_size} \
+                            --l1i_size=${l1i_size} \
+                            --l2_size=${l2_size} \
+                            --l3_size=${L3_SIZE_KB} \
+                            --l1d_assoc=${l1d_assoc} \
+                            --l1i_assoc=${l1i_assoc} \
+                            --l2_assoc=${l2_assoc} \
+                            --l3_assoc=${l3_assoc} \
+                            --network=${NETWORK} \
+                            --buffer-size=${BUFFER_SIZE} \
+                            --link-width-bits=${LINKWIDTH} \
+                            --simple-ext-link-bw-factor=$(($LINKWIDTH/8)) \
+                            --simple-int-link-bw-factor=$(($LINKWIDTH/8)) \
+                            --vcs-per-vnet=${VC_PER_VNET} \
+                            --link-latency=${LINK_LAT} \
+                            --router-latency=${ROUTER_LAT} \
+                            --topology=CustomMesh \
+                            --simple-physical-channels \
+                            --chi-config=${GEM5_DIR}/configs/example/noc_config/Starlink2.0_4x4Mesh.py \
+                            --ruby \
+                            --maxloads=${LoadFactor} \
+                            --mem-size="16GB" \
+                            --size-ws=${WKSET} \
+                            --mem-type=DDR4_3200_8x8 \
+                            --addr-mapping="RoRaBaBg1CoBg0Co53Dp" \
+                            --mem-test-type=$BENCHMARK \
+                            --addr-intrlvd-or-tiled=$MultiCoreAddrMode  \
+                            --disable-gclk-set \
+                            --enable-DMT=${DMT} \
+                            --enable-DCT=${DCT} \
+                            --num-HNF-TBE=${HNF_TBE} \
+                            --ratio-repl-req-TBE=$PART_RATIO \
+                            --part-TBEs=$PART_TBE \
+                            --num-SNF-TBE=${SNF_TBE}  \
+                            --sequencer-outstanding-requests=${SEQ_TBE} \
+                            --num_trans_per_cycle_llc=${TRANS} \
+                            --num-cpus=${NUMCPUS} \
+                            --num-dmas=0 \
+                            --inj-interval=1 \
+                            --num-snoopfilter-entries=${SNOOP_FILTER_SIZE} \
+                            --num-snoopfilter-assoc=${SNOOP_FILTER_ASSOC} \
+                            --allow-infinite-SF-entries=${IDEAL_SNOOP_FILTER} \
+                            --xor-addr-bits=${XOR_ADDR_BITS} \
+                            --block-stride-bits=${BLOCK_STRIDE_BITS} \
+                            --randomize-acc=${RANDOMIZE_ACC} \
+                            --ratio-read-write=${READ_WRITE_RATIO} \
+                            --slots_bocked_by_set=${SLOTS_BLOCKED_BY_SET} \
+                            --chi-buffer-depth=16 \
+                            --chi-buffer-max-deq-rate=1 \
+                            --num-producers=1 > ${OUTPUT_DIR}/cmd.log 2>&1 &
                     done
                 done
             done
         done
+        wait
+        echo "------------------------------------------------"
     done
 
-    # for snf in $(seq 0 3); do
-    #     grep -E "system.ruby.snf${snf}.cntrl" debug.trace > debug.snf${snf}.trace
-    # done
+    ${PY3} stats_parser.py --root-dir ${OUTPUT_ROOT}/${OUTPUT_PREFIX} \
+                           --output-file ${OUTPUT_ROOT}/${OUTPUT_PREFIX}/stats.csv \
+
 fi
---
+++ stats_parser.py
@@ -0,0 +1,180 @@
+import re
+import os
+import ast
+import copy
+import json
+import logging
+import argparse
+import numpy as np
+import pprint as pp
+import pandas as pd
+
+def getAllStatsDir(root_dir):
+    # Recursive function to find all files named "stats.txt" in the folder and its subfolders
+    def find_output_dirs(folder):
+        output_dirs = []
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                if file == "stats.txt":
+                    # Check if the filepath contains "CHECKPNT"
+                    if "CHECKPNT" not in root:
+                        output_dirs.append(root)
+                    else:
+                        logging.debug(f'Skip: {root}')
+        return output_dirs
+
+    # Call the function to find all "stats.txt" files in "ARM_FS" folder and filter out files with "CHECKPNT" in their path
+    output_dirs = find_output_dirs(root_dir)
+    return output_dirs
+
+
+def variableFileParser(args, variableFilePath):
+    all_var_dict = {}
+    variableFilePath = os.path.join(variableFilePath,'Variables.txt')
+    with open(variableFilePath, 'r') as f:
+        variables = f.readlines()
+    for var in variables:
+        kvpair= var.strip().split('=')
+        if len(kvpair) == 2:
+            all_var_dict[kvpair[0]]=kvpair[1]
+
+    var_list = {
+        'BENCHMARK':str,
+        'NUMCPUS':int,
+        'NUM_LLC':int,
+        'WKSET': int,
+        # 'RESTORE_CPU':str,
+        # 'SNPFILTER_ENTRIES':str,
+        'NUM_MEM':int,
+        # 'NUM_DDR_Side':int,
+        # 'NUM_DDR_XP':int,
+        'DCT':ast.literal_eval,
+        'DMT':ast.literal_eval,
+        'HNF_TBE':int,
+        'OUTPUT_DIR': str
+        # 'SNF_TBE':int,
+        # 'SEQ_TBE':int,
+        # 'TRANS':int,
+        # 'LINKWIDTH':int,
+        # 'VC_PER_VNET':int,
+        # 'BUFFER_SIZE':int,
+        # 'NETWORK':str,
+        # 'l1d_size':str,
+        # 'l1i_size':str,
+        # 'l2_size':str,
+        # 'l3_size':str,
+        # 'LLC_REPL':str
+    }
+
+    var_dict = dict()
+    for key,ops in var_list.items():
+        try:
+            var_dict[key]=ops(all_var_dict.get(key))
+        except:
+            pass
+    args = copy.deepcopy(args)
+    for key,val in var_dict.items():
+        setattr(args, key, val)
+    return args
+
+def getReadWriteStats(options):
+    gen_memcpy_bw = (options.BENCHMARK == 'memcpy_test')
+    readsPat        = re.compile(r'system.cpu(\d*).numReads( +)(\d+)')
+    writesPat       = re.compile(r'system.cpu(\d*).numWrites( +)(\d+)')
+    tickPerCycPat   = re.compile(r'system.clk_domain.clock( +)(\d+)')
+    simTicksPat     = re.compile(r'simTicks( +)(\d+)')
+    hnfHitPat       = re.compile(r'system.ruby.hnf(\d*).cntrl.cache.m_demand_hits( +)(\d+)')
+    hnfMissPat      = re.compile(r'system.ruby.hnf(\d*).cntrl.cache.m_demand_misses( +)(\d+)')
+    reqTbeSizePat   = re.compile(r'system.ruby.hnf(\d*).cntrl.avg_size( +)(\d*[.,]?\d*)( +)# TBE Request Occupancy')
+    replTbeSizePat  = re.compile(r'system.ruby.hnf(\d*).cntrl.avg_size( +)(\d*[.,]?\d*)( +)# TBE Repl Occupancy')
+    hnfRetryAckPats = re.compile(r'system.ruby.hnf(\d*).cntrl.cache.m_RetryAcks( +)(\d+)')
+    snfRetryAckPats = re.compile(r'system.ruby.snf(\d*).cntrl.rspOut.m_retry_msgs( +)(\d+)')
+    snfTbeSizePat   = re.compile(r'system.ruby.snf(\d*).cntrl.avg_size( +)(\d+)')
+    numReads        = -1
+    numWrites       = -1
+    tickPerCyc      = -1
+    cyc             = 0
+    simTicks        = 0
+    statsFile       = os.path.join(options.OUTPUT_DIR, 'stats.txt')
+    numHNFHits      = 0
+    numHNFMisses    = 0
+    reqTbeUtil      = 0
+    replTbeSizeUtil = 0
+    hnfRetryAcks    = 0
+    snfRetryAcks    = 0
+    snfSizeUtil     = 0
+    with open(statsFile,'r') as sf:
+        for line in sf:
+            readsMatch=readsPat.search(line)
+            writesMatch=writesPat.search(line)
+            tickPerCycMatch=tickPerCycPat.search(line)
+            simTickMatch=simTicksPat.search(line)
+            hnfHitMatch=hnfHitPat.search(line)
+            hnfMissMatch=hnfMissPat.search(line)
+            reqTbeUtilMatch=reqTbeSizePat.search(line)
+            hnfRetryAckMatch=hnfRetryAckPats.search(line)
+            snfRetryAckMatch=snfRetryAckPats.search(line)
+            replTbeSizeMatch=replTbeSizePat.search(line)
+            snfTbeSizeMatch=snfTbeSizePat.search(line)
+            if readsMatch :
+                numReads += int(readsMatch.group(3))
+            elif writesMatch :
+                numWrites += int(writesMatch.group(3))
+            elif tickPerCycMatch :
+                tickPerCyc = int(tickPerCycMatch.group(2))
+            elif simTickMatch :
+                simTicks = int(simTickMatch.group(2))
+            elif hnfHitMatch :
+                numHNFHits += int(hnfHitMatch.group(3))
+            elif hnfMissMatch :
+                numHNFMisses += int(hnfMissMatch.group(3))
+            elif reqTbeUtilMatch :
+                reqTbeUtil += float(reqTbeUtilMatch.group(3))
+            elif replTbeSizeMatch :
+                replTbeSizeUtil += float(replTbeSizeMatch.group(3))
+            elif hnfRetryAckMatch:
+                hnfRetryAcks += int(hnfRetryAckMatch.group(3))
+            elif snfRetryAckMatch:
+                snfRetryAcks += int(snfRetryAckMatch.group(3))
+            elif snfTbeSizeMatch:
+                snfSizeUtil += int(snfTbeSizeMatch.group(3))
+    assert(tickPerCyc > 0)
+    cyc=simTicks/tickPerCyc
+    bw=64*(numReads+numWrites)/cyc
+    if gen_memcpy_bw :
+        bw=(64*numReads)/cyc
+    totalHNFAcc=numHNFHits+numHNFMisses
+    hnfMissRate=-1
+    replTbeSizeUtil=replTbeSizeUtil/(options.NUM_LLC)
+    reqTbeUtil=reqTbeUtil/(options.NUM_LLC)
+    snfSizeUtil=snfSizeUtil/(options.NUM_MEM)
+    if totalHNFAcc > 0:
+        hnfMissRate=numHNFMisses/totalHNFAcc
+
+    return pd.DataFrame({
+        'benchname': [options.BENCHMARK],
+        'working_set': [options.WKSET],
+        'num_cpus': [options.NUMCPUS],
+        'num_llc': [options.NUM_LLC],
+        'hnf_TBE': [options.HNF_TBE],
+        'hnfMissRate': hnfMissRate,
+        'bw': bw
+    }, index = None)
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--root-dir', required=True)
+    parser.add_argument('--output-file', required=True)
+    options=parser.parse_args()
+    
+    simDirList = getAllStatsDir(options.root_dir)
+    simDataDict = {simDir: variableFileParser(options, simDir) for simDir in simDirList}
+    df = pd.DataFrame()
+    for _, simDataArgs in simDataDict.items(): 
+        df = pd.concat([df, getReadWriteStats(simDataArgs)])
+
+    with open(options.output_file, 'w') as f:
+        df.to_csv(f, index=False, header=True)
+
+if __name__=="__main__":
+    main()
